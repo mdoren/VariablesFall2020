69eb6ca5b3b69e34457e7db858c0f4bad7ca0224
Matt Doren <mattdoren62@gmail.com>
2020-09-14 14:01:40 -0500

    Initial commit

---
+++ src/main.kt
@@ -0,0 +1,20 @@
+fun main(){
+    //Assign values (because they do not change) to each different type of variables we're demoing
+    val myByte: Byte = 0
+    val myShort: Short = 2
+    val myInt: Int = 62
+    val myLong: Long = 41000000000L
+    val myFloat: Float = 3.141592653F
+    val myDouble = 839.22
+    val myBoolean= true
+    val myChar: Char = 'L'
+    //Print the values
+    println("My byte: $myByte")
+    println("My short: $myShort")
+    println("My Int: $myInt")
+    println("My Long: $myLong")
+    println("My Float: $myFloat")
+    println("My Double: $myDouble")
+    println("My Boolean: $myBoolean")
+    println("My Char: $myChar")
+}
---
+++ src/practice.kt
@@ -0,0 +1,27 @@
+fun main(args: Array<String>){
+    //Numbers
+    var myInt: Int = 10
+    var myLong = 40L
+    var myFloat: Float = 3.14F
+    var myDouble: Double = 45.2356765
+
+    var anotherInt = 12
+
+    //Booleans
+    var myBoolean: Boolean = true
+    myBoolean = false
+
+    //Strings
+    var myString1 = "This is a string."
+    var myString2 = "This string has a newline \n"
+    var multiLines = """
+        This is line 1
+        This is line 2
+        This is line 3
+    """.trimIndent()
+
+    val acctBalance = 200
+    val mybal = "My balance is $acctBalance"
+
+
+}
